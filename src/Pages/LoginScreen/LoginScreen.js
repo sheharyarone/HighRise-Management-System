6bd7c66d505033a5a7bc9f11b9364bccd1b6d95e
Sheharyar <sheharyarone@gmail.com>
2022-12-13 17:19:03 +0500

    login page set

---
+++ src/Pages/LoginScreen/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoginScreen.css";
+import Login from "../../Components/Login/Login";
+function LoginScreen() {
+  return (
+    <div className="d-flex justify-content-center my-5 pt-5">
+      <Login />
+    </div>
+  );
+}
+
+export default LoginScreen;
